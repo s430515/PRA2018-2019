5c9f202a4bba99b13a4a0686892c71dd939d2428
s430515 <patgaj2@st.amu.edu.pl>
2020-03-31 23:47:15 +0200

    commit 1

--- src/main/java/pracownia/introduction/HelloWorld.java
+++ src/main/java/pracownia/introduction/HelloWorld.java
@@ -1,8 +1,14 @@
 package pracownia.introduction;
 
+import java.util.logging.Logger;
+
 public class HelloWorld {
 
+
     public static void main(String [ ] args) {
+        Logger log = Logger.getLogger("name");
+
+        log.info("message");
         System.out.println("Hello World");
     }
 }
